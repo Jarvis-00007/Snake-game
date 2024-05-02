90b2d094518366af90a217b6fd41d94c300fb3cd
Jarvis-00007 <168718686+Jarvis-00007@users.noreply.github.com>
2024-05-02 15:16:30 +0530

    Update maingame.py

--- maingame.py
+++ maingame.py
@@ -1,9 +1,21 @@
 import pygame
-class maingame
+class maingame:
   def __init__(self):
-    pass
-  def update(self):
-    pass
-  def render(self):
-    pass
+    pygame.init()
+    self.dims=(1000,500)
+    self.loop=False
+    self.window=pygame.display.set_mode(self.dims)
     
+  def update(self):
+    for event in pygame.event.get():
+      if event.type==pygame.QUIT:
+        print("entered loop")
+        self.loop=True
+      
+  def render(self):
+    self.window.fill((45,97,36))
+    pygame.display.update()
+  def play(self):
+    while not self.loop:
+      self.update()
+      self.render()
