059e01e3075afee7187d0bf601d4a58460937f27
Jarvis-00007 <168718686+Jarvis-00007@users.noreply.github.com>
2024-05-02 15:53:42 +0530

    Update maingame.py

--- maingame.py
+++ maingame.py
@@ -1,19 +1,35 @@
+from Orbs import orb
 import pygame
+pygame.init()
+num_orbs=30
+import random
 class maingame:
   def __init__(self):
-    pygame.init()
     self.dims=(1000,500)
     self.loop=False
     self.window=pygame.display.set_mode(self.dims)
-    
+    self.orbs=[]
+
+  def init(self):
+    for i in range(num_orbs):
+      randX=random.randint(0,self.dims[0])
+      randY=random.randint(0,self.dims[1])
+      randr=random.randint(0,50)
+      fruit=orb(randX,randY,randr,randr)
+      self.orbs.append(fruit)
+    self.play()
   def update(self):
     for event in pygame.event.get():
       if event.type==pygame.QUIT:
         self.loop=True
-      
+
+
   def render(self):
-    self.window.fill((45,97,36))
+    self.window.fill((48,25,52))
+    for fruit in self.orbs:
+      fruit.draw(self.window)
     pygame.display.update()
+
   def play(self):
     while not self.loop:
       self.update()
