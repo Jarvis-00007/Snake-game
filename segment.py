6e30b7a091539f751b0bc57ca9cc50095ffe0d92
hasini-j <168719566+hasini-j@users.noreply.github.com>
2024-05-04 01:07:25 +0530

    Update segment.py

--- segment.py
+++ segment.py
@@ -10,6 +10,8 @@ class Segment:
     def update(self,target):
          direction=[target[0]-self.rect.x,target[1]-self.rect.y]
          length=(direction[0]**2 +direction[1]**2)**(1/2)
+         if length<self.rect.w/2:
+             return
          direction[0] /= length
          direction[1] /= length
 
