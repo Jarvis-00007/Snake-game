b982283b3c64bdc2348444d8b64df2a98888c49b
hasini-j <168719566+hasini-j@users.noreply.github.com>
2024-05-04 00:59:04 +0530

    Create segment.py

---
+++ segment.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Segment:
+    def  __init__(self,x,y,w,h,fpath):
+        self.rect=pygame.Rect(x,y,w,h)
+        img=pygame.image.load("reddot.png")
+        self.texture=pygame.transform.scale(img,(w,h))
+        self.speed=1
+    
+    def update(self,target):
+         direction=[target[0]-self.rect.x,target[1]-self.rect.y]
+         length=(direction[0]**2 +direction[1]**2)**(1/2)
+         direction[0] /= length
+         direction[1] /= length
+
+         self.rect.x += direction[0] *self.speed
+         self.rect.y += direction[1] * self.speed
+
